0cd3c13a45cf5f26dd9cfd5b2fc7d9fc74ba38a7
AprilWong <aprilwang.zju@gmail.com>
2017-02-05 00:24:12 -0800

    navigation is working

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require('smoothscroll-polyfill').polyfill()
+
 function openNav() {
   var toggle= document.querySelector('.container')
   if(toggle.classList.contains('checked'))
@@ -6,13 +8,24 @@ function openNav() {
 }
 
 function closeNav() {
-  console.log('666')
   var toggle = document.querySelector('.container')
   if(toggle.classList.contains('checked'))
     toggle.classList.remove('checked')
 }
 
+function scrollTo(name) {
+  var nextSection = document.querySelector(name)
+  nextSection.scrollIntoView({behavior:'smooth'})
+  closeNav()
+}
+
+
 export function addListeners() {
-  document.querySelector('.toggle-icon').addEventListener('click', openNav)
+  document.querySelector('.toggle-label').addEventListener('click', openNav)
   document.querySelector('.pusher-container').addEventListener('click', closeNav) 
+  document.querySelector('#enter').addEventListener('click', function(){scrollTo('#sec1')})
+  document.querySelector('#nav1').addEventListener('click', function(){scrollTo('#sec1')})
+  document.querySelector('#nav2').addEventListener('click', function(){scrollTo('#sec2')})
+  document.querySelector('#nav3').addEventListener('click', function(){scrollTo('#sec3')})
+  document.querySelector('#nav4').addEventListener('click', function(){scrollTo('#sec4')})
 }
