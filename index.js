ec896298b08d25a742ae70c554c41b4600c21a3b
April Wang <aprilwang.zju@gmail.com>
2017-10-03 00:08:24 -0700

    add project &  more

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import {addListeners} from './src/app.js'
 import template from './src/homepage.pug'
 import data from './src/homepage.json'
 import style from './src/scss/common.scss'
+import mixitup from 'mixitup'
+
 
 let html = template({data:data})
 document.write(html)
 addListeners()
+var mixer = mixitup('.container')
