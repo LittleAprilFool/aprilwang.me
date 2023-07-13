49fdd775a3ea37219cc71087a49049ab922fd1a2
April Wang <aprilww@m-xp3q3p52gg.local>
2023-07-13 18:34:22 -0400

    fix cv title format

--- dist/bundle.js
+++ dist/bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "456c806f75182c1a5e9c";
+/******/ 	var hotCurrentHash = "c535c1b11864b08ee65a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
